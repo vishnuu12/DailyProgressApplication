30550a9feb53ace40610829be791f853d5fd4237
Vishnu <vishnudcrazie@gmail.com>
2024-10-26 16:04:30 +0530

    Changed title to BTS

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'mySimpleIonicApp';
+  title = 'BTS';
 
   constructor(){
     
